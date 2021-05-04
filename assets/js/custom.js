a0db3299032e766bf546bd5c4bc7479212d22e36
ritchbe <itchy.ooc@gmail.com>
2021-05-04 17:58:09 +0100

    updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,11 +25,11 @@ jQuery( document ).ready(function( $ ) {
           var box = $('.header-text').height();
           var header = $('header').height();
 
-          if (scroll >= 2) {
-            $("header").addClass("background-header");
-          } else {
-            $("header").removeClass("background-header");
-          }
+        //   if (scroll >= 2) {
+        //     $("header").addClass("background-header");
+        //   } else {
+        //     $("header").removeClass("background-header");
+        //   }
         });
 		if ($('.owl-testimonials').length) {
             $('.owl-testimonials').owlCarousel({
